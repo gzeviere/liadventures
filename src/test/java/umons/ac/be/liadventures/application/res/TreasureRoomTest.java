890088e825d548c7789f3482ef2d2a7c9a4df18d
gzavier <gzavier@protonmail.com>
2022-08-17 02:30:20 +0200

    final version

--- src/test/java/umons/ac/be/liadventures/application/res/TreasureRoomTest.java
+++ src/test/java/umons/ac/be/liadventures/application/res/TreasureRoomTest.java
@@ -7,31 +7,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TreasureRoomTest {
 
+    //free space
     @org.junit.jupiter.api.Test
-    void bestPossibleOutcome() {
+    void bestPossibleOutcomeFreeSpace() {
         var treasureRoom = new TreasureRoom();
         //All the elements weight maximum 5 ounces, at least one element would fit in a bag. Value of an element is greater than 0.
         assertNotEquals(0, treasureRoom.bestPossibleOutcome(5));
     }
 
+    //not overfill
     @org.junit.jupiter.api.Test
-    void bestPossibleOutcomeNotOverfill() {
+    void bestPossibleOutcomeNoOverfill() {
         var treasureRoom = new TreasureRoom();
         assertEquals(0, treasureRoom.bestPossibleOutcome(0));
     }
 
+    //not empty
     @org.junit.jupiter.api.Test
-    void bestElementOrder() {
+    void bestElementOrderNotEmpty() {
         var treasureRoom = new TreasureRoom();
         assertNotEquals(0, treasureRoom.bestElementOrder().size());
     }
 
+    //has all elements
     @org.junit.jupiter.api.Test
     void bestElementOrderHasAllElements(){
         var treasureRoom = new TreasureRoom();
         assertEquals(treasureRoom.totalElements, treasureRoom.bestElementOrder().size());
     }
 
+    //is sorted by ratio
     @org.junit.jupiter.api.Test
     void isBestElementOrderSortedByRatio(){
         var treasureRoom = new TreasureRoom();
